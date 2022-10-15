5336401916efe6ed323b94271c769df0da963249
Iwwww <mike.yolgin@gmail.com>
2022-10-15 18:45:56 +0300

    Update conifg.h

--- config.h
+++ config.h
@@ -3,14 +3,9 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("sb-disk",        1800, 10),
+	BLOCK("sb-internet",    20,   4),
+	BLOCK("sb-volume",      0,    3),
+	BLOCK("sb-battery",     5,    2),
+	BLOCK("sb-clock",       60,   1)
 };
